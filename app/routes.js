92019955e97f582319c0cd8e9971713b046de073
Alex Robertson <lex1989@gmail.com>
2016-12-13 17:49:12 +0000

    Added routes for connecting accounts

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,7 @@ module.exports = function(app, passport) {
     failureFlash: true
   }));
 
-  // SIGN UP
+  // LOCAL SIGNUP
   app.get('/signup', function(req,res) {
     res.render('signup.ejs', {
       message: req.flash('signupMessage')
@@ -30,14 +30,20 @@ module.exports = function(app, passport) {
     failureFlash: true
   }));
 
-  // PROFILE
-  app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
-      user: req.user
+  // LOCAL CONNECT
+  app.get('/connect/local', function(req, res) {
+    res.render('connect-local.ejs', {
+      message: req.flash('loginMessage')
     });
   });
 
-  // FACEBOOK
+  app.post('/connect/local', passport.authenticate('local-signup',{
+    successRedirect: '/profile',
+    failureRedirect: '/connect/local',
+    failureFlash: true
+  }));
+
+  // FACEBOOK SIGNUP
   app.get('/auth/facebook', passport.authenticate('facebook', {
     scope: 'email'
   }));
@@ -47,7 +53,17 @@ module.exports = function(app, passport) {
     failureRedirect: '/'
   }));
 
-  // TWITTER
+  // FACEBOOK CONNECT
+  app.get('/connect/facebook', passport.authorize('facebook', {
+    scope: 'email'
+  }));
+
+  app.get('/connect/facebook/callback', passport.authorize('facebook', {
+    successRedirect: '/profile',
+    failureRedirect: '/'
+  }));
+
+  // TWITTER SIGNUP
   app.get('/auth/twitter', passport.authenticate('twitter'));
 
   app.get('/auth/twitter/callback', passport.authenticate('twitter', {
@@ -55,7 +71,17 @@ module.exports = function(app, passport) {
     failureRedirect: '/'
   }));
 
-  // GOOGLE
+  // TWITTER CONNECT
+  app.get('/connect/twitter', passport.authorize('twitter', {
+    scope: 'email'
+  }));
+
+  app.get('/connect/twitter/callback', passport.authenticate('twitter', {
+    successRedirect: '/profile',
+    failureRedirect: '/'
+  }));
+
+  // GOOGLE SIGNUP
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   }));
@@ -65,13 +91,30 @@ module.exports = function(app, passport) {
     failureRedirect: '/'
   }));
 
-  // TWITCH
+  // GOOGLE CONNECT
+  app.get('/connect/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+  app.get('/auth/google/callback', passport.authenticate('google', {
+    successRedirect: '/profile',
+    failureRedirect: '/'
+  }));
+
+  // TWITCH SIGNUP
   app.get('/auth/twitch', passport.authenticate('twitch'));
 
   app.get('/auth/twitch/callback', passport.authenticate('twitch', {
     successRedirect: '/profile',
     failureRedirect: '/'
   }));
+
+  // PROFILE
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user: req.user
+    });
+  });
 
   // LOG OUT
   app.get('/logout', function(req, res) {
